beb4817ef7fbb5ee8777e687f15cb667c63c36f8
Anthony Chan <anthony.chan@synpulse.com>
2022-12-27 21:53:40 +0800

    make  up login screen:

--- screens/ProfileStackScreens/LoginScreen.tsx
+++ screens/ProfileStackScreens/LoginScreen.tsx
@@ -1,150 +1,60 @@
-import React, { useEffect } from 'react'
-import {
-    Button,
-    colors,
-    size,
-    Spacing
-    } from '@starter'
-import { FontAwesome } from '@expo/vector-icons'
+import React from 'react'
+import { Controller, useForm } from 'react-hook-form'
 import { ProfileStackScreenProps } from '../../types'
-import { useDispatch, useSelector } from 'react-redux'
-import { userInfoActions, userInfoSeletor } from '@slice/userInfo'
+import { useDispatch } from 'react-redux'
+import {
+  Button,
+  FormText,
+  size,
+} from '@starter'
 import {
   Dimensions,
   StyleSheet,
-  Text,
-  TouchableOpacity,
   View,
 } from 'react-native'
 
 
 const width = Dimensions.get('window').width
 
+interface IForm {
+  identifier: string
+  password: string
+}
+
 export default function LoginScreen({
   navigation,
 }: ProfileStackScreenProps<'LoginPage'>) {
-  const dispatch = useDispatch()
-  const { user, status } = useSelector(userInfoSeletor)
+  const dispatch = useDispatch<any>()
+  const { control, handleSubmit, formState: { errors } } = useForm<IForm>({
+    defaultValues: {
+      identifier: '',
+      password: '',
+    }
+  })
 
-  const userLogout = () => {
-    dispatch(userInfoActions.logout())
-  }
-
-  const onPressProfile = () => {
-    navigation.navigate('DetailPage')
-  }
-
-  const onPressContactUs = () => {
-    navigation.navigate('ContactPage')
-  }
-
-  const onPressLogin = () => {
+  const onPressLogin = (data: IForm) => {
 
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.top}>
-        {user && <>
-          <View style={styles.circle}>
-            <FontAwesome size={30} name='plus' />
-          </View>
-          <Text style={styles.name}>{user?.username}</Text>
-        </>}
-        <View style={styles.box}>
-          <Text>Available lesson(s)</Text>
-          <Text style={styles.gray}>5</Text>
-        </View>
-        {user && <TouchableOpacity
-          style={styles.box2}
-          onPress={onPressProfile}
-        >
-          <Text>Profile</Text>
-          <Text style={styles.gray}>{'>'}</Text>
-        </TouchableOpacity>}
-        <TouchableOpacity
-          style={styles.box}
-          onPress={onPressContactUs}
-        >
-          <Text>Contact Us</Text>
-          <Text style={styles.gray}>{'>'}</Text>
-        </TouchableOpacity>
-      </View>
-      <Spacing height={size[6]} />
-      {user
-        ?
-        <Button
-          title='Log Out'
-          color={colors.danger}
-          onPress={userLogout}
-        />
-        : <>
-          <Button title='login' onPress={onPressLogin} type='outline' />
-          <Spacing height={size[4]} />
-          <Button title='register' onPress={onPressProfile} type='outline' />
-        </>}
+      <Controller control={control} name='identifier' rules={{ required: true }} render={({ field: { value, onChange } }) => {
+        return <FormText title='User Name' onChangeText={onChange} text={value} error={errors.identifier && 'This is required'} />
+      }} />
+      <Controller control={control} name='password' rules={{ required: true }} render={({ field: { value, onChange } }) => {
+        return <FormText title='Password' isPaasword onChangeText={onChange} text={value} error={errors.password && 'This is required'} />
+      }} />
+      <Button
+        title='Login'
+        onPress={handleSubmit(onPressLogin)}
+      />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  emptyContainer: {
-    flex: 1,
-  },
   container: {
     flex: 1,
     padding: size[4]
-  },
-  circle: {
-    marginTop: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 150,
-    height: 150,
-    backgroundColor: 'white',
-    borderRadius: 100,
-  },
-  name: {
-    margin: 20,
-    fontSize: 30,
-  },
-  box: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'white',
-    width: '100%',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-  },
-  box2: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    backgroundColor: 'white',
-    width: '100%',
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
-    marginTop: 25,
-  },
-  gray: {
-    color: '#757575',
-  },
-  button: {
-    width: '100%',
-  },
-  top: {
-    alignItems: 'center',
-  },
-  bottom: {
-    width: '100%',
-    alignItems: 'center',
-  },
-  empty: {
-    fontSize: 20,
   },
 })
